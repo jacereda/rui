92cdb72a4d15e0eb3ad6401a6478c4b96a00feae
Jorge Acereda <jacereda@gmail.com>
2021-10-02 13:06:11 +0200

    Increase write/read size

--- msg.h
+++ msg.h
@@ -1,14 +1,20 @@
 #define MTU 1500
+#define BATCH (MTU * 6)
+static inline size_t
+szmin(size_t a, size_t b)
+{
+	return a < b ? a : b;
+}
+
 static inline void
 msgsend(const uint8_t *p, size_t s)
 {
 	size_t sofar = 0;
 	while (sofar < s) {
-		const size_t mtu = MTU;
-		if (sendto(g_sock, p + sofar, mtu, 0,
+		if (sendto(g_sock, p + sofar, szmin(BATCH, s - sofar), 0,
 			(const struct sockaddr *)&g_sin, sizeof(g_sin)) < 0)
 			perror("sendto");
-		sofar += mtu;
+		sofar += BATCH;
 	}
 }
 
@@ -17,10 +23,10 @@ msgrecv(uint8_t *p, size_t s)
 {
 	size_t sofar = 0;
 	while (sofar < s) {
-		const size_t mtu = MTU;
-		socklen_t    sinlen;
-		ssize_t	     rs = recvfrom(g_sock, p + sofar, mtu, MSG_WAITALL,
-			 (struct sockaddr *)&g_sin, &sinlen);
+		socklen_t sinlen;
+		ssize_t	  rs =
+		    recvfrom(g_sock, p + sofar, szmin(BATCH, s - sofar),
+			MSG_WAITALL, (struct sockaddr *)&g_sin, &sinlen);
 		if (rs < 0)
 			perror("recvfrom");
 		sofar += rs;
